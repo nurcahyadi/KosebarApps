e1e46bf5ae19508ae2afe5bb23df85ddef50dec6
nurcahyadi <perdana.nurcahyadi@gmail.com>
2018-02-17 11:04:20 +0700

    second commit

--- app/src/main/java/com/example/android/kosebar/MainActivity.java
+++ app/src/main/java/com/example/android/kosebar/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.android.kosebar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSignOut = findViewById(R.id.btnsignout);
+
+
+        mSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(a);
+                finish();
+            }
+        });
     }
 }
